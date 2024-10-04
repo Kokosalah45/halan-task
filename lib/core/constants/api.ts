429af75ea8dc8d6d334aa83bc17898e228f4f774
kero <kokp.amged@gmail.com>
2024-10-04 06:01:13 +0300

    chore: scaffolded the project

---
+++ lib/core/constants/api.ts
@@ -0,0 +1,4 @@
+import { env } from "../env.mjs";
+
+// for the future, so we can account for multiple environments
+const BASE_URL = env.NEXT_PUBLIC_BASE_URL;
---
+++ lib/core/constants/validation.ts
@@ -0,0 +1,3 @@
+export const ACCESS_TOKEN_REGEX = new RegExp(
+  "eyJ[.A-Za-z0-9_-]{2,}.[A-Za-z0-9_-]{2,}.[A-Za-z0-9_-]{2,}",
+);
