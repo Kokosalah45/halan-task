d666bf6108e3c3dba46aad57f4399f19810e9472
kero <kokp.amged@gmail.com>
2024-10-06 13:41:32 +0300

    refactor(style): change flex layout in header to grid

--- lib/core/components/layout/Header.tsx
+++ lib/core/components/layout/Header.tsx
@@ -2,26 +2,46 @@ import React from "react";
 
 type HeaderLayoutProps = {
   startContent?: React.ReactNode;
-  endContent?: React.ReactNode;
   centerContent?: React.ReactNode;
+  endContent?: React.ReactNode;
 };
-
-const DefaulStartContent = () => <div />;
-const DefaultEndContent = () => <div />;
-const DefaultCenterContent = () => <div />;
 
 // consider using grid to remove Default components -- probably want be needed as making a sublist reponsive will use the inner element not the outer layout one
 export default function HeaderLayout({
-  startContent = <DefaulStartContent />,
-  centerContent = <DefaultCenterContent />,
-  endContent = <DefaultEndContent />,
+  startContent,
+  centerContent,
+  endContent,
 }: HeaderLayoutProps) {
   return (
     <header className="sticky top-0 z-50 bg-white/60">
-      <div className="flex justify-between items-center layout ">
-        {startContent}
-        {centerContent}
-        {endContent}
+      <div className="grid grid-cols-3 justify-between items-center layout ">
+        <div
+          className="
+            col-start-1
+            col-span-1
+            justify-self-start
+          "
+        >
+          {startContent}
+        </div>
+        <div
+          className="
+            col-start-2
+            col-span-1
+            justify-self-center
+          "
+        >
+          {centerContent}
+        </div>
+        <div
+          className="
+            col-start-3
+            col-span-1
+            justify-self-end
+          "
+        >
+          {endContent}
+        </div>
       </div>
     </header>
   );
